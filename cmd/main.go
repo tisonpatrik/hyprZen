4a40f5469183bcb7e14ec2b448ca8d83c73cd447
tisonpatrik <patriktison@gmail.com>
2025-07-26 01:27:23 +0200

    bug fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"hyprzen/internal"
-	"log"
 )
 
 func main() {
-	if err := internal.PreInstallSetup(); err != nil {
-		log.Fatalf("PreInstallSetup failed: %v", err)
-	}
-	if err := internal.InstallSystem(); err != nil {
-		log.Fatalf("InstallSystem failed: %v", err)
-	}
-	if err := internal.InstallAps(); err != nil {
-		log.Fatalf("InstallAps failed: %v", err)
-	}
-	if err := internal.AddConfigs(); err != nil {
-		log.Fatalf("AddConfigs failed: %v", err)
-	}
-	
-	fmt.Println("✅ All installation steps completed successfully!")
+	internal.PreInstallSetup()
+	internal.InstallSystem()
+	internal.InstallAps()
+	internal.AddConfigs()
 }
