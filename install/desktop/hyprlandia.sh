b340bba42a727e9bf8967a8fe25122fb645135b4
Jaxon Wright <jaxonhwright@gmail.com>
2025-08-03 04:26:59 -0400

    Add Hyprsunset for Blue Light Filtering / Nightlight (#447)
    * install hyprsunset when installing other hypr packages
    
    * add hyprsunset to autostart
    
    * add default hyprsunset config to make it do nothing by default
    
    * waybar: add hyprsunset shortcut next to bluetooth shortcut to quickly toggle blue light filtering
    
    * waybar: fix icon switching for hyprsunset widget
    
    * add migration for hyprsunset
    
    * waybar: rename "Blue Light Filter" to "Nightlight"
    
    * waybar: make nightlight module appear next to bluetooth module
    
    * waybar: do not include nightlight module by default
    
    * add omarchy-toggle-nightlight script
    
    * add default keybinding for toggling hyprsunset nightlight (SUPER + CTRL + N)
    
    * waybar: remove nightlight module from config entirely
    
    * waybar: remove nightlight from css
    
    * update hyprsunset migration to skip yay install if already installed
    
    * omarchy-toggle-nightlight: use `omarchy-restart-waybar` script instead of pkill with -SIG
    
    * omarchy-toggle-nightlight: only restart waybar if nightlight is in the waybar config
    
    ---------
    
    Co-authored-by: David Heinemeier Hansson <david@hey.com>

--- install/desktop/hyprlandia.sh
+++ install/desktop/hyprlandia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yay -S --noconfirm --needed \
-  hyprland hyprshot hyprpicker hyprlock hypridle polkit-gnome hyprland-qtutils \
+  hyprland hyprshot hyprpicker hyprlock hypridle hyprsunset polkit-gnome hyprland-qtutils \
   walker-bin libqalculate waybar mako swaybg swayosd \
   xdg-desktop-portal-hyprland xdg-desktop-portal-gtk \
   wayland-utils
